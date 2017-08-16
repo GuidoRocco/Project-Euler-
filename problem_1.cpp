ea3879fcf58f721527a62e6c52cbb65567df0f73
Guido Rocco <guido.rocco@hotmail.it>
2017-08-16 23:34:11 +0200

    Create problem_1.cpp

---
+++ problem_1.cpp
@@ -0,0 +1,19 @@
+/* 
+Project Euler 1 
+
+Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+*/ 
+#include <iostream> 
+using namespace std; 
+
+int main(void) 
+{
+	int sum = 0;  
+	for(int i = 1; i <= 1000; i++) 
+	{
+		if(i % 3 == 0 || i % 5 == 0) 
+		  sum += i; 
+	} 
+	cout << sum << endl; 
+	return 0; 
+}
