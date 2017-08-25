eef9ce166464831468ecb75ca4eb8083f4ace73d
Guido Rocco <guido.rocco@hotmail.it>
2017-08-25 11:24:45 +0200

    Update problem_40.cpp

--- problem_40.cpp
+++ problem_40.cpp
@@ -5,7 +5,7 @@ Problem: An irrational decimal fraction is created by concatenating the positive
 
 It can be seen that the 12th digit of the fractional part is 1.
 
-If dn represents the nth digit of the fractional part, find the value of the following expression.
+If dn represents the nth digit of the fractional part, find the value of the following expression: 
 
 d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000. 
 
