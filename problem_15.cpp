d870fffe5e893d8bdbecaf8c78a8f25b76a564de
Guido Rocco <guido.rocco@hotmail.it>
2017-08-28 12:01:32 +0200

    Update problem_15.cpp

--- problem_15.cpp
+++ problem_15.cpp
@@ -2,7 +2,7 @@
 Problem: Starting in the top left corner of a 2×2 grid, and only being able
 to move to the right and down, there are exactly 6 routes to the bottom right corner. 
 How many such routes are there through a 20×20 grid? 
-The solution of this problem can be obtained using binomial coefficient. 
+The solution of this problem can be obtained using binomial coefficients. 
 An explanation of my solution can be retrieved here: http://www.robertdickau.com/lattices.html
 */ 
 
